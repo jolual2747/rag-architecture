9cfdbc456b3bfc54dd9bbf3dbc4794d4a52179f2
Jose Alcocer <jolual2747@gmail.com>
2024-03-29 23:58:18 -0500

    developed gradio app for movies embeddings

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,11 +3,37 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
 import nltk
+from typing import List
+from chromadb.api.models.Collection import Collection
+import pandas as pd
+
 nltk.download('stopwords')
 nltk.download('punkt')
 
-def clean_text(sentence_batch) -> dict:
+def download_nltk_resources(resources: List[str]) -> None:
+    """Downloads nltk resources if not exist.
 
+    Args:
+        resources: Resources list to download.
+    """
+    
+    for resource in resources:
+        try:
+            nltk.data.find(resource)
+        except:
+            nltk.download(resource)
+
+def clean_text(sentence_batch) -> dict:
+    """Cleans a batch of data.
+
+    Args:
+        sentence_batch: Batch of data.
+
+    Returns:
+        Dict with "text": and cleaned data.
+    """
+
+    download_nltk_resources(resources=["corpora/stopwords", "tokenizers/punkt"])
     # Extracts text from the batch
     text_list = sentence_batch['text']
 
@@ -39,3 +65,37 @@ def clean_text(sentence_batch) -> dict:
         cleaned_text_list.append(filtered_text)
 
     return {'text': cleaned_text_list}
+
+def make_a_query(
+        query: str,
+        db: Collection,
+        n_results: int = 3
+) -> pd.DataFrame:
+    """Makes a query into Embedding Database through semantic search. 
+
+    Args:
+        query: Query as string.
+        db: Collection (Embedding database).
+        n_results: Number of results to retrieve. Defaults to 3.
+
+    Returns:
+        DataFrame with results.
+    """
+    results = db.query(
+        query_texts=[query],
+        n_results=n_results
+    )
+
+    response_data = []
+
+    for response in results["metadatas"][0]:
+        response_data.append(
+            {
+                'Title' : response["movie title"],
+                'Overview' : response["Overview"],
+                'Director' : response["Director"],
+                'Genre' : response["Generes"],
+                'Rating' : response["Rating"]
+            }
+        )
+    return pd.DataFrame.from_records(response_data)
